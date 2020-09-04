6b118276cafbfdeec05642023a504b6c8353b9a6
Alex Sarnese <40846040+TelepathicGrunt@users.noreply.github.com>
2020-09-03 20:03:34 -0400

    fabric branch made

--- src/main/java/com/telepathicgrunt/blame/Blame.java
+++ src/main/java/com/telepathicgrunt/blame/Blame.java
@@ -1,18 +1,15 @@
 package com.telepathicgrunt.blame;
 
-import net.minecraftforge.fml.common.Mod;
+import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod(Blame.MODID)
-public class Blame
+public class Blame implements ModInitializer
 {
-    // Directly reference a log4j logger.
-
     public static final String MODID = "blame";
     public static final Logger LOGGER = LogManager.getLogger();
 
-    public Blame() {
+    @Override
+    public void onInitialize() {
     }
 }
--- src/main/java/com/telepathicgrunt/blame/mixin/BiomeMixin.java
+++ src/main/java/com/telepathicgrunt/blame/mixin/BiomeMixin.java
@@ -4,19 +4,17 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.mojang.serialization.JsonOps;
 import com.telepathicgrunt.blame.Blame;
-import net.minecraft.crash.CrashReport;
-import net.minecraft.util.SharedSeedRandom;
+import net.minecraft.util.crash.CrashReport;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.registry.DynamicRegistries;
+import net.minecraft.util.registry.DynamicRegistryManager;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.ChunkRegion;
 import net.minecraft.world.biome.Biome;
-import net.minecraft.world.gen.ChunkGenerator;
-import net.minecraft.world.gen.WorldGenRegion;
+import net.minecraft.world.gen.ChunkRandom;
+import net.minecraft.world.gen.StructureAccessor;
+import net.minecraft.world.gen.chunk.ChunkGenerator;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
-import net.minecraft.world.gen.feature.DecoratedFeatureConfig;
-import net.minecraft.world.gen.feature.FeatureSpreadConfig;
-import net.minecraft.world.gen.feature.structure.Structure;
-import net.minecraft.world.gen.feature.structure.StructureManager;
+import net.minecraft.world.gen.feature.StructureFeature;
 import org.apache.logging.log4j.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -36,29 +34,29 @@ public class BiomeMixin {
 	 * Prints registry name of feature and biome.
 	 * Prints the crashlog to latest.log as well.
 	 */
-	@Inject(method = "func_242427_a(Lnet/minecraft/world/gen/feature/structure/StructureManager;Lnet/minecraft/world/gen/ChunkGenerator;Lnet/minecraft/world/gen/WorldGenRegion;JLnet/minecraft/util/SharedSeedRandom;Lnet/minecraft/util/math/BlockPos;)V",
-			at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/crash/CrashReport;makeCategory(Ljava/lang/String;)Lnet/minecraft/crash/CrashReportCategory;", ordinal = 1),
+	@Inject(method = "generateFeatureStep(Lnet/minecraft/world/gen/StructureAccessor;Lnet/minecraft/world/gen/chunk/ChunkGenerator;Lnet/minecraft/world/ChunkRegion;JLnet/minecraft/world/gen/ChunkRandom;Lnet/minecraft/util/math/BlockPos;)V",
+			at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/util/crash/CrashReport;create(Ljava/lang/Throwable;Ljava/lang/String;)Lnet/minecraft/util/crash/CrashReport;", ordinal = 1),
 			locals = LocalCapture.CAPTURE_FAILHARD)
-	private void addFeatureDetails(StructureManager structureManager, ChunkGenerator chunkGenerator,
-								   WorldGenRegion worldGenRegion, long seed, SharedSeedRandom random, BlockPos pos,
+	private void addFeatureDetails(StructureAccessor structureAccessor, ChunkGenerator chunkGenerator,
+								   ChunkRegion chunkRegion, long seed, ChunkRandom random, BlockPos pos,
 								   CallbackInfo ci, List<List<Supplier<ConfiguredFeature<?, ?>>>> GenerationStageList,
 								   int numOfGenerationStage, int generationStageIndex, int configuredFeatureIndex,
 								   Iterator var12, Supplier supplier, ConfiguredFeature<?, ?> configuredfeature,
 								   Exception exception, CrashReport crashreport)
 	{
-		DynamicRegistries dynamicRegistries = worldGenRegion.getWorld().getWorldServer().func_241828_r();
+		DynamicRegistryManager dynamicRegistries = chunkRegion.getRegistryManager();
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-		crashreport.getCategory()
-				.addDetail("\n****************** Blame Report ******************",
-					"\n\n ConfiguredFeature Registry Name : " + Objects.requireNonNull(dynamicRegistries.func_243612_b(Registry.field_243552_au).getKey(configuredfeature)) +
-					"\n Biome Registry Name : " + Objects.requireNonNull(dynamicRegistries.func_243612_b(Registry.BIOME_KEY).getKey((Biome)(Object)this)) +
+		crashreport.getSystemDetailsSection()
+				.add("\n****************** Blame Report ******************",
+					"\n\n ConfiguredFeature Registry Name : " + Objects.requireNonNull(dynamicRegistries.get(Registry.CONFIGURED_FEATURE_WORLDGEN).getKey(configuredfeature)) +
+					"\n Biome Registry Name : " + Objects.requireNonNull(dynamicRegistries.get(Registry.BIOME_KEY).getKey((Biome)(Object)this)) +
 					"\n\n JSON info : " +
-							(ConfiguredFeature.field_236264_b_.encode(() -> configuredfeature, JsonOps.INSTANCE, JsonOps.INSTANCE.empty()).get().left().isPresent()
-									? gson.toJson(ConfiguredFeature.field_236264_b_.encode(() -> configuredfeature, JsonOps.INSTANCE, JsonOps.INSTANCE.empty()).get().left().get())
+							(ConfiguredFeature.CODEC.encode(() -> configuredfeature, JsonOps.INSTANCE, JsonOps.INSTANCE.empty()).get().left().isPresent()
+									? gson.toJson(ConfiguredFeature.CODEC.encode(() -> configuredfeature, JsonOps.INSTANCE, JsonOps.INSTANCE.empty()).get().left().get())
 									: " failed to get json.") + "\n\n");
 
-		Blame.LOGGER.log(Level.ERROR, crashreport.getCompleteReport());
+		Blame.LOGGER.log(Level.ERROR, crashreport.getMessage());
 	}
 
 
@@ -67,24 +65,24 @@ public class BiomeMixin {
 	 * Prints registry name of feature and biome.
 	 * Prints the crashlog to latest.log as well.
 	 */
-	@Inject(method = "func_242427_a(Lnet/minecraft/world/gen/feature/structure/StructureManager;Lnet/minecraft/world/gen/ChunkGenerator;Lnet/minecraft/world/gen/WorldGenRegion;JLnet/minecraft/util/SharedSeedRandom;Lnet/minecraft/util/math/BlockPos;)V",
-			at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/crash/CrashReport;makeCategory(Ljava/lang/String;)Lnet/minecraft/crash/CrashReportCategory;", ordinal = 0),
+	@Inject(method = "generateFeatureStep(Lnet/minecraft/world/gen/StructureAccessor;Lnet/minecraft/world/gen/chunk/ChunkGenerator;Lnet/minecraft/world/ChunkRegion;JLnet/minecraft/world/gen/ChunkRandom;Lnet/minecraft/util/math/BlockPos;)V",
+			at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/util/crash/CrashReport;create(Ljava/lang/Throwable;Ljava/lang/String;)Lnet/minecraft/util/crash/CrashReport;", ordinal = 0),
 			locals = LocalCapture.CAPTURE_FAILHARD)
-	private void addStructureDetails(StructureManager structureManager, ChunkGenerator chunkGenerator,
-									 WorldGenRegion worldGenRegion, long seed, SharedSeedRandom random, BlockPos pos,
+	private void addStructureDetails(StructureAccessor structureAccessor, ChunkGenerator chunkGenerator,
+									 ChunkRegion chunkRegion, long seed, ChunkRandom random, BlockPos pos,
 									 CallbackInfo ci, List list, int numOfGenerationStage, int generationStageIndex,
-									 int configuredFeatureIndex, Iterator var12, Structure<?> structure,
+									 int configuredFeatureIndex, Iterator var12, StructureFeature<?> structureFeature,
 									 int chunkX, int chunkZ, int ChunkXPos, int ChunkZPos,
 									 Exception exception, CrashReport crashreport)
 	{
-		DynamicRegistries dynamicRegistries = worldGenRegion.getWorld().getWorldServer().func_241828_r();
+		DynamicRegistryManager dynamicRegistries = chunkRegion.getRegistryManager();
 
-		crashreport.getCategory()
-				.addDetail("\n****************** Blame Report ******************",
-				"\n\n Structure Name : " + structure.getStructureName() +
-				"\n Structure Registry Name : " + Objects.requireNonNull(dynamicRegistries.func_243612_b(Registry.STRUCTURE_FEATURE_KEY).getKey(structure)) +
-				"\n Biome Registry Name : " + Objects.requireNonNull(dynamicRegistries.func_243612_b(Registry.BIOME_KEY).getKey((Biome)(Object)this)) + "\n\n");
+		crashreport.getSystemDetailsSection()
+				.add("\n****************** Blame Report ******************",
+				"\n\n Structure Name : " + structureFeature.getName() +
+				"\n Structure Registry Name : " + Objects.requireNonNull(dynamicRegistries.get(Registry.STRUCTURE_FEATURE_KEY).getKey(structureFeature)) +
+				"\n Biome Registry Name : " + Objects.requireNonNull(dynamicRegistries.get(Registry.BIOME_KEY).getKey((Biome)(Object)this)) + "\n\n");
 
-		Blame.LOGGER.log(Level.ERROR, crashreport.getCompleteReport());
+		Blame.LOGGER.log(Level.ERROR, crashreport.getMessage());
 	}
 }
