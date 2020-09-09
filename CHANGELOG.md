5c6951727beb8288c2c9b761832c13b2af4145c3
Alex Sarnese <40846040+TelepathicGrunt@users.noreply.github.com>
2020-09-09 09:51:56 -0400

    PRINTS BROKEN JSON ON WORLD LOAD NOW

---
+++ CHANGELOG.md
@@ -0,0 +1,21 @@
+    Made for Minecraft v.1.16.2
+    Created by TelepathicGrunt
+
+Gotta blame the broken mods!
+
+------------------------------------------------
+       | Blame Forge changelog |
+
+
+   (V.1.1.0 Changes) (1.16.2 Minecraft)
+   
+    JSON file on world loading:
+• When starting up a world and a broken JSON file is found, 
+  this mod will now print the resourcelocation of the broken 
+  JSON file so it can be found and corrected faster! 
+
+
+   (V.1.0.4 Changes) (1.16.2 Minecraft)
+   
+    Major:
+• Full release of working Blame that doesn't crash itself! Wooooo!!!
---
+++ src/main/java/com/telepathicgrunt/blame/utils/PrettyPrintBrokenJSON.java
@@ -0,0 +1,94 @@
+package com.telepathicgrunt.blame.utils;
+
+
+/*
+ * Creator: Christian Morin
+ * Licensed under: CC BY-SA 3.0
+ * Source: https://stackoverflow.com/a/24605993
+ */
+public class PrettyPrintBrokenJSON {
+
+    public static String prettyPrintJSONAsString(String jsonString) {
+
+        int tabCount = 0;
+        StringBuffer prettyPrintJson = new StringBuffer();
+        String lineSeparator = "\r\n";
+        String tab = "  ";
+        boolean ignoreNext = false;
+        boolean inQuote = false;
+
+        char character;
+
+        /* Loop through each character to style the output */
+        for (int i = 0; i < jsonString.length(); i++) {
+
+            character = jsonString.charAt(i);
+
+            if (inQuote) {
+
+                if (ignoreNext) {
+                    ignoreNext = false;
+                } else if (character == '"') {
+                    inQuote = false;
+                }
+                prettyPrintJson.append(character);
+            } else {
+
+                if (ignoreNext) {
+                    ignoreNext = !ignoreNext;
+                }
+
+                switch (character) {
+
+                    case '[':
+                    case '{':
+                        ++tabCount;
+                        prettyPrintJson.append(character);
+                        prettyPrintJson.append(lineSeparator);
+                        printIndent(tabCount, prettyPrintJson, tab);
+                        break;
+
+                    case ']':
+                    case '}':
+                        --tabCount;
+                        prettyPrintJson.append(lineSeparator);
+                        printIndent(tabCount, prettyPrintJson, tab);
+                        prettyPrintJson.append(character);
+                        break;
+
+                    case '"':
+                        inQuote = true;
+                        prettyPrintJson.append(character);
+                        break;
+
+                    case ',':
+                        prettyPrintJson.append(character);
+                        prettyPrintJson.append(lineSeparator);
+                        printIndent(tabCount, prettyPrintJson, tab);
+                        break;
+
+                    case ':':
+                        prettyPrintJson.append(character).append(" ");
+                        break;
+
+                    case '\\':
+                        prettyPrintJson.append(character);
+                        ignoreNext = true;
+                        break;
+
+                    default:
+                        prettyPrintJson.append(character);
+                        break;
+                }
+            }
+        }
+
+        return prettyPrintJson.toString();
+    }
+
+    private static void printIndent(int count, StringBuffer stringBuffer, String indent) {
+        for (int i = 0; i < count; i++) {
+            stringBuffer.append(indent);
+        }
+    }
+}
