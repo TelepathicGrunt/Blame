181e8ad5d80d71aa9814a0a21a0fc144602305d2
Alex Sarnese <40846040+TelepathicGrunt@users.noreply.github.com>
2021-05-07 20:35:08 -0400

    updated env and added processor crash report

---
+++ src/main/java/com/telepathicgrunt/blame/main/ProcessorBlame.java
@@ -0,0 +1,47 @@
+package com.telepathicgrunt.blame.main;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.telepathicgrunt.blame.Blame;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.structure.Structure;
+import net.minecraft.structure.StructureManager;
+import net.minecraft.structure.StructurePlacementData;
+import net.minecraft.structure.processor.StructureProcessor;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.WorldAccess;
+import net.minecraft.world.WorldView;
+import org.apache.logging.log4j.Level;
+
+import java.util.Map;
+import java.util.Optional;
+
+/* @author - TelepathicGrunt
+ *
+ * Find why an nbt file is exploding when loading blocks from the files or why a processor is blowing up.
+ *
+ * LGPLv3
+ */
+public class ProcessorBlame {
+
+	public static Structure.StructureBlockInfo findBrokenProcessor(StructureProcessor structureProcessor, WorldView world, BlockPos blockPos1, BlockPos blockPos2, Structure.StructureBlockInfo blockInfo1, Structure.StructureBlockInfo blockInfo2, StructurePlacementData placementSettings) {
+		try{
+			return structureProcessor.process(world, blockPos1, blockPos2, blockInfo1, blockInfo2, placementSettings);
+		}
+		catch (Exception e){
+			String blockBeingProcessed = blockInfo1.state.toString();
+			String blockNBT = "";
+			if(blockInfo1.tag != null){
+				Gson gson = new GsonBuilder().setPrettyPrinting().create();
+				blockNBT = gson.toJson(blockInfo1.tag);
+			}
+
+			String errorReport = "\n****************** Blame Report " + Blame.VERSION + " ******************" +
+					"\n\n Processor blew up trying to process a block." +
+					"\n Block being processed: " + blockBeingProcessed +
+					"\n NBT of the block being processed: " + blockNBT;
+			Blame.LOGGER.log(Level.ERROR, errorReport);
+			throw e;
+		}
+	}
+}
---
+++ src/main/java/com/telepathicgrunt/blame/mixin/StructureMixin.java
@@ -0,0 +1,30 @@
+package com.telepathicgrunt.blame.mixin;
+
+import com.telepathicgrunt.blame.main.ProcessorBlame;
+import net.minecraft.structure.Structure;
+import net.minecraft.structure.StructurePlacementData;
+import net.minecraft.structure.processor.StructureProcessor;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.WorldAccess;
+import net.minecraft.world.WorldView;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+/* @author - TelepathicGrunt
+ *
+ * Find why an nbt file is exploding when loading blocks from the files or why a processor is blowing up.
+ * Can be quite useful to find which Jigsaw Block's replacement block field is mispelled.
+ *
+ * LGPLv3
+ */
+@Mixin(Structure.class)
+public class StructureMixin {
+
+	@Redirect(method = "process(Lnet/minecraft/world/WorldAccess;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/structure/StructurePlacementData;Ljava/util/List;)Ljava/util/List;",
+			at = @At(value = "INVOKE",
+			target = "Lnet/minecraft/structure/processor/StructureProcessor;process(Lnet/minecraft/world/WorldView;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/structure/Structure$StructureBlockInfo;Lnet/minecraft/structure/Structure$StructureBlockInfo;Lnet/minecraft/structure/StructurePlacementData;)Lnet/minecraft/structure/Structure$StructureBlockInfo;"))
+	private static Structure.StructureBlockInfo processBlockInfos(StructureProcessor structureProcessor, WorldView world, BlockPos blockPos1, BlockPos blockPos2, Structure.StructureBlockInfo blockInfo1, Structure.StructureBlockInfo blockInfo2, StructurePlacementData placementSettings) {
+		return ProcessorBlame.findBrokenProcessor(structureProcessor, world, blockPos1, blockPos2, blockInfo1, blockInfo2, placementSettings);
+	}
+}
