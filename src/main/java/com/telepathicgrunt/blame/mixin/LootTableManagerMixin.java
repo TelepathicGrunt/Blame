1c3a6a843fc86fcf1e889d54c38f049cd9af3efa
AlcatrazEscapee <alex@molleroneill.com>
2021-05-29 22:48:40 -0400

    Suppress unnecessary stacktraces for loot table and recipe errors as they're large and unhelpful.

--- src/main/java/com/telepathicgrunt/blame/mixin/LootTableManagerMixin.java
+++ src/main/java/com/telepathicgrunt/blame/mixin/LootTableManagerMixin.java
@@ -1,16 +1,21 @@
 package com.telepathicgrunt.blame.mixin;
 
-import com.telepathicgrunt.blame.main.MissingLoottableBlame;
+import java.util.Map;
+
+import com.google.gson.Gson;
+import org.apache.logging.log4j.Logger;
+import net.minecraft.client.resources.JsonReloadListener;
 import net.minecraft.loot.LootTable;
 import net.minecraft.loot.LootTableManager;
 import net.minecraft.util.ResourceLocation;
+
+import com.telepathicgrunt.blame.main.MissingLoottableBlame;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.Map;
 
 /* @author - TelepathicGrunt
  *
@@ -19,7 +24,12 @@ import java.util.Map;
  * LGPLv3
  */
 @Mixin(LootTableManager.class)
-public class LootTableManagerMixin {
+public abstract class LootTableManagerMixin extends JsonReloadListener
+{
+	private LootTableManagerMixin(Gson gson_, String string_)
+	{
+		super(gson_, string_);
+	}
 
 	@Shadow
 	private Map<ResourceLocation, LootTable> tables;
@@ -31,5 +41,15 @@ public class LootTableManagerMixin {
 		if(!tables.containsKey(rl)){
 			MissingLoottableBlame.addMissingLoottableDetails(rl);
 		}
+	}
+
+	/**
+	 * Log a more useful message. Full stack trace is not useful. Concise, readable errors are useful.
+	 */
+	@SuppressWarnings("UnresolvedMixinReference")
+	@Redirect(method = "*(Lnet/minecraft/resources/IResourceManager;Lcom/google/common/collect/ImmutableMap$Builder;Lnet/minecraft/util/ResourceLocation;Lcom/google/gson/JsonElement;)V", at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;error(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V"), require = 0, remap = false)
+	private void simplifyInvalidLootTableLogOutput(Logger logger, String message, Object p0, Object p1)
+	{
+		logger.error(message + " {}: {} (Blame: suppressed long stacktrace)", p0, p1.getClass().getSimpleName(), ((Exception) p1).getMessage());
 	}
 }
