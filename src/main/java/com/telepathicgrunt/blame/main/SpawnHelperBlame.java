a1790ffd370ceee23f158e81e77c57bcb2281495
Alex Sarnese <40846040+TelepathicGrunt@users.noreply.github.com>
2021-06-02 23:25:21 -0400

    fixed and added more logging for entity weights

--- src/main/java/com/telepathicgrunt/blame/main/SpawnHelperBlame.java
+++ src/main/java/com/telepathicgrunt/blame/main/SpawnHelperBlame.java
@@ -8,6 +8,7 @@ import net.minecraft.util.collection.WeightedPicker;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.ServerWorldAccess;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.SpawnSettings;
@@ -23,7 +24,7 @@ import java.util.List;
  */
 public class SpawnHelperBlame {
 
-    public static void addFeatureDetails(ServerWorld serverWorld, SpawnGroup entityClassification, BlockPos pos, Biome biome, List<SpawnSettings.SpawnEntry> list) {
+    public static void addMobCrashDetails(ServerWorld serverWorld, SpawnGroup entityClassification, BlockPos pos, Biome biome, List<SpawnSettings.SpawnEntry> list) {
         // Figure out if mob spawning is gonna crash game and to run our code if so.
         int totalWeight = WeightedPicker.getWeightSum(list);
         if (totalWeight <= 0) {
@@ -32,15 +33,37 @@ public class SpawnHelperBlame {
             Identifier biomeID = serverWorld.getRegistryManager().get(Registry.BIOME_KEY).getId(biome);
 
             // Add extra info to the log file.
-            Blame.LOGGER.log(Level.ERROR, "\n****************** Blame Report " + Blame.VERSION + " ******************",
+            Blame.LOGGER.log(Level.ERROR, "\n****************** Blame Report " + Blame.VERSION + " ******************" +
                     "\n  Detected total weight of mob list is 0 or negative which will crash the game! " +
-                            "\n  See info below to find which mob is the problem and where it is attempting to spawn at." +
-                            "\n World Registry Name : " + worldID.getValue().toString() +
-                            "\n Biome Registry Name : " + (biomeID != null ? biomeID.toString() : "Wait what? How is the biome not registered and has no registry name!?!? This should be impossible!!!") +
-                            "\n Classification of entity being spawned : " + entityClassification.getName() +
-                            "\n Entity position : " + pos.toString() +
-                            "\n Weighted list of mobs to spawn : " + printMobListContents(list) +
-                            "\n");
+                    "\n  See info below to find which mob is the problem and where it is attempting to spawn at." +
+                    "\n World Registry Name : " + worldID.getValue().toString() +
+                    "\n Biome Registry Name : " + (biomeID != null ? biomeID.toString() : "Wait what? How is the biome not registered and has no registry name!?!? This should be impossible!!!") +
+                    "\n Classification of entity being spawned : " + entityClassification.getName() +
+                    "\n Entity position : " + pos.toString() +
+                    "\n Weighted list of mobs to spawn : " + printMobListContents(list) +
+                    "\n");
+
+        }
+    }
+
+    public static void addMobCrashDetails(ServerWorldAccess serverWorld, SpawnGroup entityClassification, int chunkX, int chunkZ, Biome biome, List<SpawnSettings.SpawnEntry> list) {
+        // Figure out if mob spawning is gonna crash game and to run our code if so.
+        int totalWeight = WeightedPicker.getWeightSum(list);
+        if (totalWeight <= 0) {
+
+            RegistryKey<World> worldID = serverWorld.toServerWorld().getRegistryKey();
+            Identifier biomeID = serverWorld.getRegistryManager().get(Registry.BIOME_KEY).getId(biome);
+
+            // Add extra info to the log file.
+            Blame.LOGGER.log(Level.ERROR, "\n****************** Blame Report " + Blame.VERSION + " ******************" +
+                    "\n  Detected total weight of mob list is 0 or negative which will crash the game! " +
+                    "\n  See info below to find which mob is the problem and where it is attempting to spawn at." +
+                    "\n World Registry Name : " + worldID.getValue().toString() +
+                    "\n Biome Registry Name : " + (biomeID != null ? biomeID.toString() : "Wait what? How is the biome not registered and has no registry name!?!? This should be impossible!!!") +
+                    "\n Classification of entity being spawned : " + entityClassification.getName() +
+                    "\n Entity position :  chunk " + chunkX + " " + chunkZ +
+                    "\n Weighted list of mobs to spawn : " + printMobListContents(list) +
+                    "\n");
 
         }
     }
--- src/main/java/com/telepathicgrunt/blame/mixin/SpawnHelperMixin.java
+++ src/main/java/com/telepathicgrunt/blame/mixin/SpawnHelperMixin.java
@@ -3,6 +3,7 @@ package com.telepathicgrunt.blame.mixin;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.ServerWorldAccess;
 import net.minecraft.world.SpawnHelper;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.SpawnSettings;
@@ -11,13 +12,14 @@ import net.minecraft.world.gen.chunk.ChunkGenerator;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.List;
 import java.util.Random;
 
-import static com.telepathicgrunt.blame.main.SpawnHelperBlame.addFeatureDetails;
+import static com.telepathicgrunt.blame.main.SpawnHelperBlame.addMobCrashDetails;
 
 /* @author - TelepathicGrunt
  *
@@ -35,6 +37,15 @@ public class SpawnHelperMixin {
                                                  ChunkGenerator chunkGenerator, SpawnGroup spawnGroup,
                                                  Random random, BlockPos pos, CallbackInfoReturnable<SpawnSettings.SpawnEntry> cir,
                                                  Biome biome, List<SpawnSettings.SpawnEntry> list) {
-        addFeatureDetails(serverWorld, spawnGroup, pos, biome, list);
+        addMobCrashDetails(serverWorld, spawnGroup, pos, biome, list);
+    }
+
+    @Inject(method = "populateEntities(Lnet/minecraft/world/ServerWorldAccess;Lnet/minecraft/world/biome/Biome;IILjava/util/Random;)V",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/util/collection/WeightedPicker;getRandom(Ljava/util/Random;Ljava/util/List;)Lnet/minecraft/util/collection/WeightedPicker$Entry;", ordinal = 0),
+            locals = LocalCapture.CAPTURE_FAILHARD)
+    private static void checkIfMobSpawnWillCrash2(ServerWorldAccess serverWorldAccess, Biome biome, int chunkX, int chunkZ,
+                                                  Random random, CallbackInfo ci, SpawnSettings spawnSettings,
+                                                  List<SpawnSettings.SpawnEntry> list) {
+        addMobCrashDetails(serverWorldAccess, SpawnGroup.CREATURE, chunkX, chunkZ, biome, list);
     }
 }
