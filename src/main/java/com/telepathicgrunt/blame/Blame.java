3b8b7217c32df67324f2747e3a1d882c9d5fbb41
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2021-07-31 13:33:37 -0400

    added code biome test if needed

--- src/main/java/com/telepathicgrunt/blame/Blame.java
+++ src/main/java/com/telepathicgrunt/blame/Blame.java
@@ -2,6 +2,13 @@ package com.telepathicgrunt.blame;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.BuiltinRegistries;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.BiomeEffects;
+import net.minecraft.world.biome.GenerationSettings;
+import net.minecraft.world.biome.SpawnSettings;
+import net.minecraft.world.gen.surfacebuilder.ConfiguredSurfaceBuilders;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -17,6 +24,26 @@ public class Blame implements ModInitializer {
                 .ifPresent(container -> VERSION = container.getMetadata().getVersion().toString());
 
         Blame.LOGGER.log(Level.ERROR, "Blame " + VERSION + " initialized");
+
+
+//        Biome newBiome = (new Biome.Builder())
+//                .precipitation(Biome.Precipitation.RAIN)
+//                .category(Biome.Category.FOREST)
+//                .depth(5)
+//                .scale(5)
+//                .temperature(0.6F)
+//                .downfall(0.6F)
+//                .effects((new BiomeEffects.Builder())
+//                        .waterColor(9204)
+//                        .waterFogColor(9011)
+//                        .fogColor(8463)
+//                        .skyColor(3534).build())
+//                .spawnSettings(new SpawnSettings.Builder().build())
+//                .generationSettings((new GenerationSettings.Builder())
+//                        .surfaceBuilder(ConfiguredSurfaceBuilders.GRASS).build())
+//                .build();
+//        BuiltinRegistries.add(BuiltinRegistries.BIOME, new Identifier(Blame.MODID, "test_code_biome"), newBiome);
+
 
         // Unregistered worldgen test
 //        ConfiguredFeature<?,?> well = Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new Identifier("tgtr:fdgfg"), Feature.DESERT_WELL.configure(DefaultFeatureConfig.INSTANCE));
